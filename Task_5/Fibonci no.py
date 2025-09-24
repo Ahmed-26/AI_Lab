5512e452a6be3c67a4473e52e673673e17346304
Ahmed-26 <ahmedrasheed6008@gmail.com>
2025-09-25 00:54:58 +0500

    task_5

---
+++ Task_5/Fibonci no.py
@@ -0,0 +1,17 @@
+no_list=[0,1]
+target=52
+while True:
+    last=no_list[-1]
+    last2nd=no_list[-2]
+    next=last+last2nd
+    if next > target:
+        break
+    no_list.append(next)
+
+print(no_list)
+
+no_list=[0,1]
+for i in range(2,10):
+    no_list.append(no_list[-1] + no_list[-2])
+print(no_list)
+
